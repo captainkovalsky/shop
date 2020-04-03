bd98cd5d655cbc442d2787584e6225231a8f6fad
Viktor Dzundza <vdzundza@protonmail.com>
2020-04-03 18:52:32 +0300

    chore(): add build script
    Signed-off-by: Viktor Dzundza <vdzundza@protonmail.com>

---
+++ scripts/build.bash
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+package_name=$1
+dist=$2
+
+if [[ -z "$dist" ]]; then
+  dist="dist"
+fi
+
+
+if [[ -z "$package_name" ]]; then
+  package_name="shop"
+fi
+
+platforms=("windows/amd64" "linux/amd64" "darwin/amd64")
+
+for platform in "${platforms[@]}"
+do
+    platform_split=(${platform//\// })
+    GOOS=${platform_split[0]}
+    GOARCH=${platform_split[1]}
+    output_name=$package_name'-'$GOOS'-'$GOARCH
+    if [ $GOOS = "windows" ]; then
+        output_name+='.exe'
+    fi
+
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o "$dist/$output_name"
+    if [ $? -ne 0 ]; then
+        echo 'An error has occurred! Aborting the script execution...'
+        exit 1
+    fi
+done
