3a9bb8d6f1ab0b55ab411217a991142743a51551
Viktor Dzundza <vdzundza@protonmail.com>
2020-07-24 15:36:03 +0300

    frontend(): try to use Bulma

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,6 @@
 
 set -e
 
-echo "deploy application"
+echo "copy artifacts to the server"
 
 rsync -avz -e "ssh" --progress workspace/* $SSH_USER@$SSH_HOST:~/ci
-ssh $SSH_USER@$SSH_HOST .~/ci/scripts/deploy-services.bash
