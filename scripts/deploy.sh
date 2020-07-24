912068723e492310c080e4962e81fa7d30eb175f
Viktor Dzundza <vdzundza@protonmail.com>
2020-07-24 11:16:53 +0300

    chore(): fix known_host

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,5 @@ set -e
 
 echo "deploy application"
 
-ssh $SSH_USER@$SSH_HOST "cd ~/ci && rm *"
-
-scp workspace/back.tar $SSH_USER@$SSH_HOST:~/ci
-
-scp workspace/front.tar $SSH_USER@$SSH_HOST:~/ci
-
+#rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress workspace/* $SSH_USER@$SSH_HOST:~/ci
+rsync -avz -e "ssh" --progress workspace/* $SSH_USER@$SSH_HOST:~/ci
