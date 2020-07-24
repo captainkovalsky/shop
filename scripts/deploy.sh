d467c67941d24512be626382dceee93787d4af64
Viktor Dzundza <vdzundza@protonmail.com>
2020-07-24 10:50:35 +0300

    chore(): try to use docker

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,10 @@
 set -e
 
 echo "deploy application"
-ssh $SSH_USER@$SSH_HOST rm -rf ci
-scp workspace/back.tar $SSH_USER@$SSH_HOST:/ci
-scp workspace/front.tar $SSH_USER@$SSH_HOST:/ci
+
+ssh $SSH_USER@$SSH_HOST "cd ~/ci && rm *"
+
+scp workspace/back.tar $SSH_USER@$SSH_HOST:~/ci
+
+scp workspace/front.tar $SSH_USER@$SSH_HOST:~/ci
 
