5483a678d82d32f9be390b8ad022f7870fa3991c
Viktor Dzundza <vdzundza@protonmail.com>
2020-07-28 10:29:42 +0300

    back: add some date

--- src/api.go
+++ src/api.go
@@ -14,13 +14,20 @@ type Cv struct {
 
 func (c Cv) Load() (*models.CV, error) {
 	return &models.CV{
-		Experience: nil,
-		Firstname:  "Viktor",
-		Surname:    "Dzundza",
-		Language: []*models.Language{
-			{Name: "English", Level: models.Language_UPPER_INTERMEDIATE},
-			{Name: "Ukrainian", Level: models.Language_NATIVE},
-			{Name: "Russian", Level: models.Language_FLUENT},
+		Experience: []*models.Experience{
+			{
+				StartDate: "02 Jan 06 15:04 -0700",
+				EndDate:   "02 Jan 06 15:04 -0700",
+				Project:   nil,
+				Company:   nil,
+			},
+		},
+		Firstname: "Viktor",
+		Surname:   "Dzundza",
+		Language: []*models.CV_Language{
+			{Name: "English", Level: models.Language_UPPER_INTERMEDIATE.String()},
+			{Name: "Ukrainian", Level: models.Language_NATIVE.String()},
+			{Name: "Russian", Level: models.Language_FLUENT.String()},
 		},
 	}, nil
 }
