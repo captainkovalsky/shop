56ed5316cab173fbd4728eaa6d8cc6634999a187
Viktor Dzundza <vdzundza@protonmail.com>
2020-11-15 22:20:12 +0200

    chore(): remove auto deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,3 @@ set -e
 echo "copy artifacts to the server"
 
 rsync -avz -e "ssh" --progress workspace/* $SSH_USER@$SSH_HOST:~/ci
-
-echo "Deploy Services"
-
-sshpass -e ssh -t $SSH_USER@$SSH_HOST 'deploy-web'
