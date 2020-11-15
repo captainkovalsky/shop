8305e18a9577b2b749b0e71df8beb4eb4cf8ca49
Viktor Dzundza <vdzundza@protonmail.com>
2020-11-15 21:38:22 +0200

    chore(): install sshpass

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,4 +8,4 @@ rsync -avz -e "ssh" --progress workspace/* $SSH_USER@$SSH_HOST:~/ci
 
 echo "Deploy Services"
 
-sshpass -e ssh -t $SSH_USER@$SSH_HOST './ci/scripts/deploy-services.bash'
+sshpass -e ssh -t $SSH_USER@$SSH_HOST 'deploy-web'
